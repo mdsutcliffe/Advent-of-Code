e10ed87a5847bcaf53bc6f632499d2b8cdbcf843
Matthew Sutcliffe <mdsutcliffe5@gmail.com>
2022-12-09 16:45:12 -0500

    still stuck on 7

--- aoc06.py
+++ aoc06.py
@@ -1,17 +1,16 @@
-file = open("input06.txt", "r")
+file = open("input/input06.txt", "r")
 x = file.readline()[:-1]
 file.close()
 
-packet_size = 4
 
-for i in range(len(x)-packet_size-1):
-    if len(set(x[i:(i+packet_size)])) == packet_size:
-        print(i + packet_size)
-        break
+def find_marker(buffer, packet_size):
+    for i in range(len(buffer) - packet_size - 1):
+        if len(set(buffer[i:(i + packet_size)])) == packet_size:
+            return (i + packet_size)
 
-packet_size = 14
 
-for i in range(len(x)-packet_size-1):
-    if len(set(x[i:(i+packet_size)])) == packet_size:
-        print(i + packet_size)
-        break
+part1 = find_marker(x, 4)
+part2 = find_marker(x, 14)
+
+print("Part 1: " + str(part1))
+print("Part 2: " + str(part2))
--- aoc07.py
+++ aoc07.py
@@ -1,25 +1,23 @@
-file = open("input07.txt", "r")
+file = open("input/input07_test.txt", "r")
 file.readline()  # root directory, not needed
 lines = file.read().split("\n")
 file.close()
 
 x = ["/"]
-sizes = [0]
-locs = [0]
+level = 0
 
 i = 0
 while True:
+    if lines[i][:4] == "$ cd" and lines[i][5:] != "..":
+        print("CDIN " + lines[i])
+        x.append(lines[i][5:])
+        i += 1
+        level += 1
     if lines[i] == "$ cd ..":
         print("CDUP " + lines[i])
         x.pop()
         i += 1
-    if lines[i][:4] == "$ cd" and lines[i][5:] != "..":
-        print("CDIN " + lines[i])
-        x.append(lines[i][5:])
-        # exec()
-        locs.append([0])
-        # break
-        i += 1
+        level -= 1
     if lines[i] == "$ ls":
         print("LIST " + lines[i])
         i += 1
@@ -34,69 +32,8 @@ while True:
                 break
     if lines[i] == "":
         break
-    # print(i)
-
-    #i += 1
-    # if i == 20:
-    #     break
 print(x)
-print(locs)
+print(level)
 
+i = 0
 
-
-
-# dirs = [[]]
-# sizes = [[]]
-# location = [0]
-#
-# # exec("location.append(4)")
-# # print(location)
-#
-# for i in range(len(x)):
-#     # print(x[i])
-#     jdir = 0
-#     if x[i] == "$ ls":
-#         i += 1
-#         while x[i][0] != "$":
-#             jdir = 0
-#             if x[i][0:3] == "dir":
-#                 jdir += 1
-#                 print("DIRECTORY", x[i])
-#                 i_location = "dirs[" + "][".join([str(l) for l in location]) + "].append("
-#                 print("dirs = " + str(dirs))
-#                 print(i_location + "\"" + str(x[i].split(" ")[1]) + "\"" + ")")
-#                 exec(i_location + "\"" + str(x[i].split(" ")[1]) + "\"" + ")")
-#                 i_location = "sizes[" + "][".join([str(l) for l in location]) + "].append([])"
-#                 exec(i_location)
-#             else:
-#                 jdir += 1
-#                 print("FILE", x[i])
-#                 i_location = "sizes[" + "][".join([str(l) for l in location]) + "].append("
-#                 print("sizes = " + str(sizes))
-#                 print(i_location + str(x[i].split(" ")[0]) + ")")
-#                 exec(i_location + str(x[i].split(" ")[0]) + ")")
-#                 # exec(dirs)
-#                 # for j in location:
-#                 #     break
-#             i += 1
-#
-#         if x[i][0:4] == "$ cd":
-#             if x[i] == "$ cd ..":
-#                 location.pop()
-#             else:
-#                 location.append(jdir)
-#         i += 1
-#         # if i >= 8:
-#         #     break
-#     # if i >= 12:
-#     #     break
-#     if x[i][0:4] == "$ cd":
-#         if x[i] == "$ cd ..":
-#             location.pop()
-#         else:
-#             location.append(jdir)
-#     i += 1
-# print("---------------")
-# print(dirs)
-# print(sizes)
-# print(location)
