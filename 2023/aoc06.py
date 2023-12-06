0ed35a9f4b5155a8c27561d7f5bf8700104e2e3b
Matthew Sutcliffe <mdsutcliffe5@gmail.com>
2023-12-06 10:43:01 -0500

    Create aoc06.py

---
+++ 2023/aoc06.py
@@ -0,0 +1,56 @@
+import math
+
+# file = open("input/input06_test.txt", "r")
+file = open("input/input06.txt", "r")
+x = file.read().split("\n")
+file.close()
+
+times = [int(i) for i in x[0].split()[1:]]
+distances = [int(i) for i in x[1].split()[1:]]
+
+
+def quadratic_formula(a, b, c):
+  return (-1 * b + (b ** 2 - 4 * a * c) ** 0.5) / (2 * a), \
+         (-1 * b - (b ** 2 - 4 * a * c) ** 0.5) / (2 * a)
+
+
+def n_ways_to_win(time, distance):
+  winning_times = sorted(quadratic_formula(-1, time, -distance))
+  min_time = math.floor(winning_times[0])+1
+  max_time = math.ceil(winning_times[1])-1
+  return max_time - min_time + 1
+
+
+def part1():
+  y = 1
+  for race in range(len(times)):
+    y = y * n_ways_to_win(times[race], distances[race])
+  return y
+
+
+def part2():
+  time_cat = int("".join([str(i) for i in times]))
+  distance_cat = int("".join([str(i) for i in distances]))
+  return n_ways_to_win(time_cat, distance_cat)
+
+
+
+# def part1():
+#   y = 1
+#   for race in range(len(times)):
+#     wins = [i for i in range(times[race]) if (times[race] - i) * i > distances[race]]
+#     y = y * len(wins)
+#   return y
+#
+
+
+# def part2():
+#   time_all = int("".join([str(i) for i in times]))
+#   distance_all = int("".join([str(i) for i in distances]))
+#   wins = [i for i in range(time_all) if (time_all - i) * i > distance_all]
+#   return len(wins)
+
+
+print(part1())
+
+print(part2())
