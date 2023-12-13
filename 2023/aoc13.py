7fccf83abdf44952577fe6117900811b3c5e214c
Matthew Sutcliffe <mdsutcliffe5@gmail.com>
2023-12-13 10:05:44 -0500

    Update aoc13.py

--- 2023/aoc13.py
+++ 2023/aoc13.py
@@ -5,56 +5,42 @@ file.close()
 x = [i.split("\n") for i in x]
 
 
-def find_reflection(x):
-  for i_r, r_i in enumerate(x[:-1]):
-    if r_i == x[i_r + 1]:
-      j = 1
+def find_reflection(x, smudges):
+  for i_row, _ in enumerate(x[:-1]):
+    errors_total = 0
+    errors_i = sum([x[i_row][i] != x[i_row + 1][i] for i, _ in enumerate(x[i_row])])
+    if errors_i <= smudges:
+      errors_total += errors_i
+      offset_row = 1
       flag = True
-      while i_r - j >= 0 and i_r + j + 1 < len(x):
-        if x[i_r - j] != x[i_r + j + 1]:
-          flag = False
-          break
-        j += 1
-      if flag:
-        return (i_r + 1)
+      while i_row - offset_row >= 0 and i_row + offset_row + 1 < len(x):
+        if x[i_row - offset_row] != x[i_row + offset_row + 1]:
+          if errors_total >= smudges:
+            flag = False
+            break
+          else:
+            errors_total += sum([x[i_row - offset_row][i] != x[i_row + offset_row + 1][i] for i, _ in enumerate(x[i_row])])
+        offset_row += 1
+      if flag and errors_total == smudges:
+        return i_row + 1
   return 0
 
 
 def part1():
   points = []
   for x_i in x:
-    points.append(find_reflection(x_i) * 100)
-    x_i_t = ["".join(i) for i in list(map(list, zip(*x_i)))]
-    points.append(find_reflection(x_i_t))
+    points.append(find_reflection(x_i, 0) * 100)
+    x_i_transpose = ["".join(i) for i in list(map(list, zip(*x_i)))]
+    points.append(find_reflection(x_i_transpose, 0))
   return sum(points)
-
-
-def find_reflection_smudge(x):
-  for i_r, r_i in enumerate(x[:-1]):
-    n_errors = 0
-    if sum([x[i_r][i] != x[i_r + 1][i] for i in range(len(x[0]))]) <= 1:
-      n_errors += sum([x[i_r][i] != x[i_r + 1][i] for i in range(len(x[0]))])
-      j = 1
-      flag = True
-      while i_r - j >= 0 and i_r + j + 1 < len(x):
-        if x[i_r - j] != x[i_r + j + 1]:
-          if n_errors >= 1:
-            flag = False
-            break
-          else:
-            n_errors += sum([x[i_r - j][i] != x[i_r + j + 1][i] for i in range(len(x[0]))])
-        j += 1
-      if flag and n_errors == 1:
-        return (i_r + 1)
-  return 0
 
 
 def part2():
   points = []
   for x_i in x:
-    points.append(find_reflection_smudge(x_i) * 100)
+    points.append(find_reflection(x_i, 1) * 100)
     x_i_t = ["".join(i) for i in list(map(list, zip(*x_i)))]
-    points.append(find_reflection_smudge(x_i_t))
+    points.append(find_reflection(x_i_t, 1))
   return sum(points)
 
 
